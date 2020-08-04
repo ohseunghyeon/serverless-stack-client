18060cb042a5703935f59cf4b61b80baa252026c
victor.oh <victor.oh@adriel.ai>
2020-08-04 17:13:21 +0900

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "victor-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://c6lc4amm8i.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_Y2Hiydifb",
+    APP_CLIENT_ID: "2efcbb6kd5ctl7tu79s7r02fp5",
+    IDENTITY_POOL_ID: "us-east-2:8aa443d4-9341-49da-9384-0948ed236fbd"
+  }
+};
